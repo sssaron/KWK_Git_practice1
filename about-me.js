a7ad32e7dde36e7590df4a7c70b62f2f48a6d13b
sssaron <hatseysaron@gmail.com>
2025-07-07 20:08:36 -0700

    Added javascript and finalized design

---
+++ about-me.js
@@ -0,0 +1,9 @@
+function funFact1() {
+  alert("The most recent album I listened to was HEIS by Rema");
+}
+function funFact2() {
+  alert("I'm allergic to caffine <///3 and I've never had an energy drink");
+}
+function funFact3() {
+  alert("I did theatre for 3 years! My most memorable role was Amy march in little women");
+}
